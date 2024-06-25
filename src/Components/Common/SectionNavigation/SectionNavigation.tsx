f95a57693250c326e52bcbe21bd3ddbc65dfae39
tienhg2001 <tien195164@huce.edu.vn>
2024-06-25 16:39:14 +0700

    feature/fix_bug-board-page

--- src/Components/Common/SectionNavigation/SectionNavigation.tsx
+++ src/Components/Common/SectionNavigation/SectionNavigation.tsx
@@ -4,23 +4,27 @@ import type { MenuItemType } from "../../../Constants/MenuItem";
 import "./SectionNavigation.scss";
 
 type Props = {
-  childrens: MenuItemType[] | undefined;
+  childrens?: MenuItemType[] | undefined;
   childrenActive?: MenuItemType;
 };
 
 const SectionNavigation: FC<Props> = ({ childrens, childrenActive }) => {
-  const navigation = useNavigate()
-
+  const navigation = useNavigate();
 
   return (
-    <div className="section_navigation-content">
+    <div
+      className={`grid-template-columns-${
+        childrens !== undefined && childrens.length >= 4 ? 4 : childrens?.length
+      } section_navigation-content`}
+    >
       {childrens &&
         childrens.map((item, index) => (
           <div
             key={index}
             id={`section_navigation-item-${index}`}
-            className={`section_navigation-item ${childrenActive?.path === item.path ? "active" : ""
-              }`}
+            className={`section_navigation-item  ${
+              childrenActive?.path === item.path && "active"
+            }`}
             onClick={() => navigation(item.path)}
           >
             <div className="section_navigation-text">
