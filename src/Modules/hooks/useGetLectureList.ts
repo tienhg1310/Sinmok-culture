f23f03b4aa6008d25bf354b898e10294509d8019
tienhg2001 <tien195164@huce.edu.vn>
2024-06-28 10:31:32 +0700

    feature/deploy

--- src/Modules/hooks/useGetLectureList.ts
+++ src/Modules/hooks/useGetLectureList.ts
@@ -2,33 +2,39 @@ import { useEffect, useState } from "react";
 import lectureService from "../API/Lecture.service";
 import { usePanigation } from "./usePanigation";
 import type { ILecture } from "../../Constants/interface";
+import fakeDB from "../../db.json"
 
 const useGetLectureList = (getAll: boolean) => {
   const [lectureList, setLectureList] = useState<ILecture[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
-  const { page, setPage, perPage, totalPage, setTotalPage, totalItem, setTotalItem, handleChangePerPage } =
+  const { page, setPage, perPage,setPerPage, totalPage, setTotalPage, totalItem, setTotalItem, handleChangePerPage } =
     usePanigation();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const params: any = {};
-        if (page) {
-          params["_page"] = page;
-        }
-        if (perPage && getAll) {
-          params["_perPage"] = perPage;
-        }
-        const headers = {};
-        const response = await lectureService.getLectures(params, headers);
+        // const params: any = {};
+        // if (page) {
+        //   params["_page"] = page;
+        // }
+        // if (perPage && getAll) {
+        //   params["_perPage"] = perPage;
+        // }
+        // const headers = {};
+        // const response = await lectureService.getLectures(params, headers);
         // setdata
-        if (response.data) {
-          setLectureList(response.data.data);
-          setTotalPage(response.data.pages);
-          setTotalItem(response.data.items);
-        }
+        // if (response.data) {
+        //   setLectureList(response.data.data);
+        //   setTotalPage(response.data.pages);
+        //   setTotalItem(response.data.items);
+        // }
+        const response = fakeDB.Lectures;
+        setLectureList(response);
+        setTotalPage(1);
+        setTotalItem(response.length);
+        // setPerPage(response.length);
       } catch (error: any) {
         setError(error.message);
       } finally {
