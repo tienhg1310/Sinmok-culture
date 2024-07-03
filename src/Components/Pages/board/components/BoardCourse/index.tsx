df51227fbbbd1d26163b8b42e6144e91414a4620
Tien Nguyen <tien195164@huce.edu.vn>
2024-07-03 10:07:39 +0700

    fix arrow and fix input stuff

--- src/Components/Pages/board/components/BoardCourse/index.tsx
+++ src/Components/Pages/board/components/BoardCourse/index.tsx
@@ -1,62 +1,55 @@
-import React from 'react'
-import Board from '../..'
-import Slider from '../../../../Common/Slider'
-import '../BoardCourse/BoardCourse.scss';
-import IconCheck from '../../../../../Assets/images/icons/IconCheck';
-import Search from '../Search';
-import SliderBoardCourse from './components/SliderBoardCourse';
-import ListBoardCourse from './components/ListBoardCourse';
-import Panigation from '../../../../Common/Panigation/Panigation';
+import React, { useState } from "react";
+import Board from "../..";
+import Slider from "../../../../Common/Slider";
+import "../BoardCourse/BoardCourse.scss";
+import IconCheck from "../../../../../Assets/images/icons/IconCheck";
+import Search from "../Search";
+import SliderBoardCourse from "./components/SliderBoardCourse";
+import ListBoardCourse from "./components/ListBoardCourse";
+import Panigation from "../../../../Common/Panigation/Panigation";
 
 export default function BoardCourse() {
+  const options = ["전체"];
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [selectedDropdown, setSelectedDropdown] = useState("");
 
-    return (
-        <Board >
-            <div className="board_course_container">
-                <div className="board_wrapper">
-                    <Slider
-                        pagination={{ clickable: true }}
-                        navigation={true}
-                        spaceBetween={50}
-                        slidesPerView={1}
-                        draggable={{ clickable: true }}
-                        autoplay={{ delay: 2000, disableOnInteraction: false }}
-                        loop={true}
-                    >
-                        {SliderBoardCourse()}
-                        <button className='swipper_btn'>
-                            <a href="/2">
-                                나의 수강후기 작성
-                                <span>
-                                    <IconCheck />
-                                </span>
-                            </a>
-                        </button>
-                    </Slider>
-
-                </div>
-            </div>
-            <div className="board_search_container">
-                <Search
-                    title='전체 12건'
-                    options={["전체"]}
-                    selectedOption={"전체"}
-                    setSelectedOption={() => { }}
-                    selectedDropdown={"전체"}
-                    setSelectedDropdown={() => { }}
-                />
-            </div>
-            <div className='board_course_list'>
-                {ListBoardCourse()}
-            </div>
-            <Panigation
-                totalPage={1}
-                page={1}
-                setPage={() => { }}
-                perPage={20}
-                setPerPage={() => { }}
-                totalItem={20}
-            />
-        </Board>
-    )
+  return (
+    <Board>
+      <div className="board_course_container">
+        <div className="board_wrapper">
+          <Slider
+            pagination={{ clickable: true }}
+            navigation={true}
+            spaceBetween={50}
+            slidesPerView={1}
+            draggable={{ clickable: true }}
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            loop={true}
+          >
+            {SliderBoardCourse()}
+            <button className="swipper_btn">
+              <a href="/2">
+                나의 수강후기 작성
+                <span>
+                  <IconCheck />
+                </span>
+              </a>
+            </button>
+          </Slider>
+        </div>
+      </div>
+      <div className="board_search_container">
+        <Search
+          title="전체 12건"
+          options={options}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+          selectedDropdown={selectedDropdown}
+          setSelectedDropdown={setSelectedDropdown}
+        />
+      </div>
+      <div className="board_course_list">{ListBoardCourse()}</div>
+      <Panigation totalPage={1} page={1} setPage={() => {}} perPage={20} setPerPage={() => {}} totalItem={20} />
+    </Board>
+  );
 }
