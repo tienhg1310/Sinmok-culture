4f3278ce423f3af0161ab082d1965d05db703ae8
phạm bảo <phambao11062001@gmail.com>
2024-07-03 20:49:06 +0700

    slider autoplay

--- src/Components/Pages/board/components/BoardCourse/index.tsx
+++ src/Components/Pages/board/components/BoardCourse/index.tsx
@@ -20,7 +20,7 @@ export default function BoardCourse() {
                         spaceBetween={50}
                         slidesPerView={1}
                         draggable={{ clickable: true }}
-                        // autoplay={{ delay: 2000, disableOnInteraction: false }}
+                        autoplay={{ delay: 2000, disableOnInteraction: false }}
                         loop={true}
                     >
                         {SliderBoardCourse()}
